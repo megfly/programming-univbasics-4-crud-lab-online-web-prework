260dc064ad04e3b34801f630f17a8badfa604bde
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-07 01:48:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,8 +36,9 @@ def retrieve_first_element_from_array(array)
   retrieve_first_element_from_array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def  retrieve_first_element_from_array(array)
+   retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   
 end
 
 def update_element_from_index(array, index_number, element)
