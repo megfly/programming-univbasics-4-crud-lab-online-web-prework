ad5873b00142cad9854db4448eb4c6c01b1005bf
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-07 01:02:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [tacos, margaritas, elote, beans]
+  ["tacos", "margaritas", "elote", "beans"]
 end
 
 def add_element_to_end_of_array(array, element)
